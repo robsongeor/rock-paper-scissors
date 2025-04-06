937c67af0e467791590c70bd79c10924e06f9580
George R <robsongeorge@hotmail.com>
2025-04-06 17:10:11 +1200

    Added computer generated choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+function getComputerChoice(){
+    // Gets a random number between 1 & 3
+    let rand3 = Math.ceil(3 * Math.random());
+
+    if (rand3 == 1){
+        return "Rock"
+    } else if (rand3 == 2){
+        return "Paper"
+    }else{
+        return "Scissors"
+    }
+}
+
+
+
